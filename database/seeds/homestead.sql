087172933e5108b4f126c5cccd58c9b4bf775ed9
指尖上的光阴 <zoudan916@163.com>
2016-08-28 11:46:52 -0400

    no commit message

--- database/seeds/homestead.sql
+++ database/seeds/homestead.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50547
 File Encoding         : 65001
 
-Date: 2016-08-26 00:37:29
+Date: 2016-08-28 23:49:15
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -51,8 +51,32 @@ CREATE TABLE `admin_users` (
 -- ----------------------------
 -- Records of admin_users
 -- ----------------------------
-INSERT INTO `admin_users` VALUES ('1', 'admin', 'admin@admin.com', '$2y$10$66x80Mfjhj4dLCRtmvmSHOPDDtoXoSjJ5zo92nfFqmcPPX7Go/yoW', 'kMoYS0gE8bXtF1MMPOl6YLgyD8EX44KPNNkrnFffhuBCbnGefvqZQZh6UeEO', '2016-05-25 05:56:33', '2016-08-26 00:10:32');
-INSERT INTO `admin_users` VALUES ('4', 'admin916', 'zoudan916@163.com', '$2y$10$y2ncKaFg33g9fGhP/Vym7OMDMKCG62INduI/43fnE5d9E/tisK3Vu', 'CxrnTlqQo2dnhkiY8av3wduVWMyCPMnk79DCTH4QPlWhCmAFHtJwY7ex7SM5', '2016-08-18 12:41:21', '2016-08-26 00:36:01');
+INSERT INTO `admin_users` VALUES ('1', 'admin', 'admin@admin.com', '$2y$10$66x80Mfjhj4dLCRtmvmSHOPDDtoXoSjJ5zo92nfFqmcPPX7Go/yoW', '5S5vX8vgKBiq9yvnJtohcN6eDtxT12jTD1iDKpPi0I9RPK3lKEWqS1iYF4xz', '2016-05-25 05:56:33', '2016-08-28 15:51:52');
+INSERT INTO `admin_users` VALUES ('4', 'admin916', 'zoudan916@163.com', '$2y$10$y2ncKaFg33g9fGhP/Vym7OMDMKCG62INduI/43fnE5d9E/tisK3Vu', 'kUTOXRvq07gwo2uICVmt30QQzG5MlWRrP9jsJyze3WUqkdZ19wKmZtpLzqjR', '2016-08-18 12:41:21', '2016-08-28 15:53:34');
+
+-- ----------------------------
+-- Table structure for article
+-- ----------------------------
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE `article` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `title` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '文章标题',
+  `bewrite` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '描述',
+  `author` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '作者',
+  `remark` text CHARACTER SET utf8 NOT NULL COMMENT '内容',
+  `scan_num` int(11) NOT NULL DEFAULT '1' COMMENT '浏览量',
+  `user_id` int(11) NOT NULL COMMENT '发布者ID',
+  `created_at` datetime NOT NULL COMMENT '创建时间',
+  `updated_at` datetime NOT NULL COMMENT '修改时间',
+  `sort` int(11) NOT NULL DEFAULT '0' COMMENT '序号',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=gbk;
+
+-- ----------------------------
+-- Records of article
+-- ----------------------------
+INSERT INTO `article` VALUES ('3', '网易新闻', '在线考试', 'admin123', 'zaixiansdfssaf', '0', '1', '2016-08-28 23:22:49', '2016-08-28 23:22:56', '0');
+INSERT INTO `article` VALUES ('4', '石柱在线', '在线提问', 'admin33', 'asdasdasd1212e', '0', '1', '2016-08-28 23:22:51', '2016-08-28 23:22:58', '0');
 
 -- ----------------------------
 -- Table structure for migrations
@@ -101,7 +125,7 @@ CREATE TABLE `permissions` (
   `icon` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `permissions_name_unique` (`name`)
-) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of permissions
@@ -121,8 +145,10 @@ INSERT INTO `permissions` VALUES ('17', 'admin.role.edit', '编辑角色', '', '
 INSERT INTO `permissions` VALUES ('18', 'admin.role.destroy', '删除角色', '', '2016-05-27 10:58:48', '2016-05-27 10:58:48', '5', null);
 INSERT INTO `permissions` VALUES ('19', 'admin.article.manage', '内容管理', '文章内容', '2016-08-22 18:22:01', '2016-08-22 18:22:01', '0', 'fa-sliders');
 INSERT INTO `permissions` VALUES ('20', 'admin.article.index', '文章列表', '', '2016-08-22 18:23:37', '2016-08-22 18:23:37', '19', null);
-INSERT INTO `permissions` VALUES ('21', 'admin.article.add', '添加文章', '', '2016-08-23 12:48:27', '2016-08-23 12:48:27', '19', null);
+INSERT INTO `permissions` VALUES ('21', 'admin.article.create', '添加文章', '', '2016-08-23 12:48:27', '2016-08-28 21:25:22', '19', null);
 INSERT INTO `permissions` VALUES ('22', 'admin.news.index', '新闻列表', '', '2016-08-23 12:56:52', '2016-08-23 12:56:52', '19', null);
+INSERT INTO `permissions` VALUES ('23', 'admin.article.edit', '修改文章', '修改', '2016-08-28 23:42:34', '2016-08-28 23:42:34', '19', null);
+INSERT INTO `permissions` VALUES ('24', 'admin.article.destroy', '删除文章', '删除', '2016-08-28 23:43:39', '2016-08-28 23:43:39', '19', null);
 
 -- ----------------------------
 -- Table structure for permission_role
@@ -147,6 +173,9 @@ INSERT INTO `permission_role` VALUES ('12', '5');
 INSERT INTO `permission_role` VALUES ('13', '5');
 INSERT INTO `permission_role` VALUES ('14', '5');
 INSERT INTO `permission_role` VALUES ('15', '5');
+INSERT INTO `permission_role` VALUES ('20', '6');
+INSERT INTO `permission_role` VALUES ('21', '6');
+INSERT INTO `permission_role` VALUES ('22', '6');
 
 -- ----------------------------
 -- Table structure for roles
@@ -161,12 +190,13 @@ CREATE TABLE `roles` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `roles_name_unique` (`name`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
 -- Records of roles
 -- ----------------------------
 INSERT INTO `roles` VALUES ('5', '高级管理员', '高级管理员', '高级管理员', '2016-08-18 12:41:02', '2016-08-18 12:41:02');
+INSERT INTO `roles` VALUES ('6', '初级管理', '初级管理', '初级', '2016-08-28 22:22:24', '2016-08-28 22:22:24');
 
 -- ----------------------------
 -- Table structure for role_user
